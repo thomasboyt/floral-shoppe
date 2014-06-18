fd52326816079fd367b22d84d08af17728ce74c4
Thomas Boyt <thomas.boyt@venmo.com>
2014-06-18 18:46:32 -0400

    Remove unneeded comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,3 @@
-// this is what's up: https://github.com/andrewrjones/grunt-ssh
-// figure out how to deploy yo shit to /home/thomas/floral-shoppe on digital ocean
-// THEN figure out how to serve that off of nginx w/o breaking dokku (/home/dokku/disco.zone./nginx.conf)
-
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
